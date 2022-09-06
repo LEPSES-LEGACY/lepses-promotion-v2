d97eacdfb2d24c7ff0e26429520f3b6d4e2f2855
Utkarsh Dwivedi <68611209+Utkarsh742@users.noreply.github.com>
2022-09-07 00:56:37 +0530

    Unanimated Website
    Promotional website

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Cards from './Components/Cards/Cards';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar'
+import Team from './Components/Team/Team';
+import Usp from './Components/Usp/Usp';
+
+
+function App() {
+
+  return (
+    <div className="App">
+     <Navbar/>
+     <Home/>
+     <Cards/>
+     <Usp/>
+     <Team/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
